3e439a46fe8c2ee1fb548d7dadabf51dc12cdd7d
Eric Chiang <Eric.Ti.Yu.Chiang@gmail.com>
2013-10-09 07:21:42 -0400

    First commit and README update

---
+++ README.md
@@ -0,0 +1,6 @@
+# Bloccit
+
+A Reddit-style application to demonstrate foundation programming and web
+developemnt skills.
+
+Made with my mentor, Joost Baaij at [Bloc](http://bloc.io)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloccit::Application.config.secret_token = '2438473085f8cf3ab8521539ddf4eca91cded860993319735925aa23972bdfc0b9797e24c41b212bcd3f0b00dfa8b015dde28e58eae2f0f6a97685ab58b586f1'
