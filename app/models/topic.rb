648297973061abcdb37906bd5d83d1d3b851becb
Eric Chiang <Eric.Ti.Yu.Chiang@gmail.com>
2013-10-20 11:26:53 -0400

    try out simple form and show validation errors for topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,7 @@
 class Topic < ActiveRecord::Base
   attr_accessible :description, :name, :public
   has_many :posts
+
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimum: 10 }, presence: true
 end
